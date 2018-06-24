222e56a48b55abc0e12044bd256ab2bca1ab01a6
kaiyuliu <1070600924@qq.com>
2018-06-24 13:08:44 +0800

    用户界面 - 商品收藏

--- routes/web.php
+++ routes/web.php
@@ -26,12 +26,14 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
      
     Route::group(['middleware' => 'email_verified'], function() {
-       Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
-       Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
-       Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
-       Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
-       Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
-       Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+      Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+      Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+      Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+      Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
+      Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
+      Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+      Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+      Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
     });
     
 });
