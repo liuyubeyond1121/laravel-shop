19d7aeb02669e0702631744b00d2611625211e38
kaiyuliu <1070600924@qq.com>
2018-06-24 19:01:55 +0800

    商品加入购物车

--- routes/web.php
+++ routes/web.php
@@ -25,15 +25,16 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
      
     Route::group(['middleware' => 'email_verified'], function() {
-      Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
-      Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
-      Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
-      Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
-      Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
-      Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
-      Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
-      Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
-      Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+        Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+        Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+        Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+        Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
+        Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
+        Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+        Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+        Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+        Route::post('cart', 'CartController@add')->name('cart.add');
     });    
 });
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
