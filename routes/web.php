d038f80f0a98556c9468c160b15a758ecfba3fd5
kaiyuliu <1070600924@qq.com>
2018-06-24 18:50:52 +0800

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,7 @@ Auth::routes();
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
 
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
@@ -34,6 +33,7 @@ Route::group(['middleware' => 'auth'], function() {
       Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
       Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
       Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
-    });
-    
+      Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+    });    
 });
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
