ee74a8beb0f1c1d06aa9c0d52783b02348dfd65e
kaiyuliu <1070600924@qq.com>
2018-09-29 23:39:09 +0800

    用户界面 - 检查优惠券

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -26,5 +26,5 @@ Route::group([
     $router->get('coupon_codes/{id}/edit', 'CouponCodesController@edit');
     $router->put('coupon_codes/{id}', 'CouponCodesController@update');
     $router->delete('coupon_codes/{id}', 'CouponCodesController@destroy');
-    
+
 });
