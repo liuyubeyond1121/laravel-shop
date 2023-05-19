28f3b310cd486f24abed382545a4fd30a095c592
liuyubeyond1121 <liuyubeyond1121@gmail.com>
2023-05-19 22:37:52 +0800

    集成支付宝

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,24 +17,24 @@ class AppServiceProvider extends ServiceProvider
     {
         // 往服务容器中注入一个名为 alipay 的单例对象
         $this->app->singleton('alipay', function () {
-            $config = config('pay.alipay');
+            $config = config('pay');
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
                 $config['mode']         = 'dev';
-                $config['log']['level'] = Logger::DEBUG;
+                $config['logger']['level'] = Logger::DEBUG;
             } else {
-                $config['log']['level'] = Logger::WARNING;
+                $config['logger']['level'] = Logger::WARNING;
             }
             // 调用 Yansongda\Pay 来创建一个支付宝支付对象
             return Pay::alipay($config);
         });
 
         $this->app->singleton('wechat_pay', function () {
-            $config = config('pay.wechat');
+            $config = config('pay');
             if (app()->environment() !== 'production') {
-                $config['log']['level'] = Logger::DEBUG;
+                $config['logger']['level'] = Logger::DEBUG;
             } else {
-                $config['log']['level'] = Logger::WARNING;
+                $config['logger']['level'] = Logger::WARNING;
             }
             // 调用 Yansongda\Pay 来创建一个微信支付对象
             return Pay::wechat($config);
--- config/pay.php
+++ config/pay.php
@@ -1,23 +1,95 @@
 <?php
 
+use Yansongda\Pay\Pay;
+
 return [
     'alipay' => [
-        'app_id'         => '',
-        'ali_public_key' => '',
-        'private_key'    => '',
-        'log'            => [
-            'file' => storage_path('logs/alipay.log'),
+        'default' => [
+            // 必填-支付宝分配的 app_id
+            'app_id' => env('ALIPAY_APP_ID'),
+            // 必填-应用私钥 字符串或路径
+            'app_secret_cert' => env('ALIPAY_APP_SECRET_CERT'),
+            // 必填-应用公钥证书 路径
+            'app_public_cert_path' => resource_path("cert/appPublicCert.crt"),
+            // 必填-支付宝公钥证书 路径
+            'alipay_public_cert_path' => resource_path('cert/alipayPublicCert.crt'),
+            // 必填-支付宝根证书 路径
+            'alipay_root_cert_path' => resource_path('cert/alipayRootCert.crt'),
+            'return_url' => 'https://yansongda.cn/alipay/return',
+            'notify_url' => 'https://yansongda.cn/alipay/notify',
+            // 选填-第三方应用授权token
+            'app_auth_token' => '',
+            // 选填-服务商模式下的服务商 id，当 mode 为 Pay::MODE_SERVICE 时使用该参数
+            'service_provider_id' => '',
+            // 选填-默认为正常模式。可选为： MODE_NORMAL, MODE_SANDBOX, MODE_SERVICE
+            'mode' => Pay::MODE_SANDBOX,
+        ]
+    ],
+    'wechat' => [
+        'default' => [
+            // 必填-商户号，服务商模式下为服务商商户号
+            'mch_id' => '',
+            // 必填-商户秘钥
+            'mch_secret_key' => '',
+            // 必填-商户私钥 字符串或路径
+            'mch_secret_cert' => '',
+            // 必填-商户公钥证书路径
+            'mch_public_cert_path' => '',
+            // 必填
+            'notify_url' => '',
+            // 选填-公众号 的 app_id
+            'mp_app_id' => '',
+            // 选填-小程序 的 app_id
+            'mini_app_id' => '',
+            // 选填-app 的 app_id
+            'app_id' => '',
+            // 选填-合单 app_id
+            'combine_app_id' => '',
+            // 选填-合单商户号
+            'combine_mch_id' => '',
+            // 选填-服务商模式下，子公众号 的 app_id
+            'sub_mp_app_id' => '',
+            // 选填-服务商模式下，子 app 的 app_id
+            'sub_app_id' => '',
+            // 选填-服务商模式下，子小程序 的 app_id
+            'sub_mini_app_id' => '',
+            // 选填-服务商模式下，子商户id
+            'sub_mch_id' => '',
+            // 选填-微信公钥证书路径, optional，强烈建议 php-fpm 模式下配置此参数
+            'wechat_public_cert_path' => [
+                '45F59D4DABF31918AFCEC556D5D2C6E376675D57' => __DIR__ . '/Cert/wechatPublicKey.crt',
+            ],
+            // 选填-默认为正常模式。可选为： MODE_NORMAL, MODE_SERVICE
+            'mode' => Pay::MODE_NORMAL,
         ],
     ],
-
-    'wechat' => [
-        'app_id'      => '',
-        'mch_id'      => '',
-        'key'         => '',
-        'cert_client' => '',
-        'cert_key'    => '',
-        'log'         => [
-            'file' => storage_path('logs/wechat_pay.log'),
+    'unipay' => [
+        'default' => [
+            // 必填-商户号
+            'mch_id' => '',
+            // 必填-商户公私钥
+            'mch_cert_path' => '',
+            // 必填-商户公私钥密码
+            'mch_cert_password' => '000000',
+            // 必填-银联公钥证书路径
+            'unipay_public_cert_path' => '',
+            // 必填
+            'return_url' => '',
+            // 必填
+            'notify_url' => '',
         ],
+    ],
+    'http' => [ // optional
+        'timeout' => 5.0,
+        'connect_timeout' => 5.0,
+        // 更多配置项请参考 [Guzzle](https://guzzle-cn.readthedocs.io/zh_CN/latest/request-options.html)
+    ],
+    // optional，默认 warning；日志路径为：sys_get_temp_dir().'/logs/yansongda.pay.log'
+    'logger' => [
+        'enable' => true,
+        'file' => storage_path('logs/alipay.log'),
+        'level' => 'debug',
+        'type' => 'single', // optional, 可选 daily.
+        'max_file' => 30,
     ],
 ];
