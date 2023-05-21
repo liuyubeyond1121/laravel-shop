d33b6e52c63970ad09bfdcb4be65e54f450726bb
liuyubeyond1121 <liuyubeyond1121@gmail.com>
2023-05-21 16:38:28 +0800

    完善支付回调

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-// 在之前的路由后面配上中间件
-Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
+
+Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes(['verify' => true]);
 
@@ -31,5 +31,5 @@ Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('pa
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
-Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
+//Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
 
