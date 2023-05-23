8579c8209363e5038161bcc2bbf1a048bf537f19
liuyubeyond1121 <liuyubeyond1121@gmail.com>
2023-05-23 20:50:25 +0800

    用户界面 - 使用优惠券下单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\CouponCodeUnavailableException;
 use App\Http\Requests\OrderRequest;
+use App\Models\CouponCode;
 use App\Models\UserAddress;
 use App\Models\Order;
 use App\Services\OrderService;
@@ -15,12 +17,26 @@ use App\Http\Requests\ApplyRefundRequest;
 
 class OrdersController extends Controller
 {
+    /**
+     * @throws CouponCodeUnavailableException
+     * @throws InvalidRequestException
+     */
     public function store(OrderRequest $request, OrderService $orderService)
     {
         $user    = $request->user();
         $address = UserAddress::find($request->input('address_id'));
+        $coupon = null;
 
-        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
+        // 如果用户提交了优惠码
+        if ($code = $request->input('coupon_code')) {
+            $coupon = CouponCode::where('code', $code)->first();
+            if (!$coupon) {
+                throw new CouponCodeUnavailableException('优惠券不存在');
+            }
+        }
+        // 参数中加入 $coupon 变量
+        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'), $coupon);
+
     }
 
     public function index(Request $request)
